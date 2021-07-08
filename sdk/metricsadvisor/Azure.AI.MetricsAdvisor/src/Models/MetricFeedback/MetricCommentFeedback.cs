a9fb5dacfcb17485215c39ecb4f5cb281fa00b24
Caio Saldanha <camaiaor@microsoft.com>
2021-07-08 11:24:11 -0700

    [MetricsAdvisor] Renamed all occurrences of StartTime to StartsOn, and EndTime to EndsOn (#22523)

--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Models/MetricFeedback/MetricCommentFeedback.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Models/MetricFeedback/MetricCommentFeedback.cs
@@ -51,12 +51,14 @@ namespace Azure.AI.MetricsAdvisor
         /// <summary>
         /// The start timestamp of feedback time range.
         /// </summary>
-        public DateTimeOffset? StartTime { get; set; }
+        [CodeGenMember("StartTime")]
+        public DateTimeOffset? StartsOn { get; set; }
 
         /// <summary>
-        /// The end timestamp of feedback timerange. When this is equal to <see cref="StartTime"/> it indicates a single timestamp.
+        /// The end timestamp of feedback timerange. When this is equal to <see cref="StartsOn"/> it indicates a single timestamp.
         /// </summary>
-        public DateTimeOffset? EndTime { get; set; }
+        [CodeGenMember("EndTime")]
+        public DateTimeOffset? EndsOn { get; set; }
 
         /// <summary>
         /// The comment content for the feedback.
